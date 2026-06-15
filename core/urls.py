f728e9360d57b5e456fe8c012037aa2a15ddeb95
Aishwarya <aishwaryak72005@gmail.com>
2026-06-15 17:41:35 +0530

    SEO: Add sitemap.xml, robots.txt, and structured data

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript'), name='sw.js'),
     path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
+    # SEO files
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots_txt'),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml'), name='sitemap_xml'),
     path('', include('users.urls')),
     path('', include('recipes.urls')),
-]
+]
